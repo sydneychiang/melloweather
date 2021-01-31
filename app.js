7947c198c80daeaf475daa43360b569055b0c238
Jon Estrada <jonathan.estrada747@gmail.com>
2021-01-31 07:54:37 -0800

    Deploy website

--- app.js
+++ app.js
@@ -4,19 +4,26 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const router = require("./router");
+const path = require("path")
 
 const app = express();
 const port = 8080;
 
 // Middleware
+
+app.use(express.static(path.join(__dirname, '../melloweather/build')))
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(cookieParser());
 
+app.use("/", router);  
 
-app.use("/", router);   
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/../melloweather/build/'))
+// })
 
 app.listen(port, function() {
     console.log(`App running on port ${port}...`);
